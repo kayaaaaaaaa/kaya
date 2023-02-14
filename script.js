be7ab515f6bec7e711ec093db82243db31d425cc
kayaaaaaaaa <125378105+kayaaaaaaaa@users.noreply.github.com>
2023-02-14 20:41:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,17 @@
-const kaustav = "Kaustav";
-const yashika = "Yashika";
-
-const loveWords = [
-	"love",
-	"heart",
-	"passion",
-	"desire",
-	"romance",
-	"forever",
-	"eternity",
-	"soulmate",
-	"beloved",
-	"cherish",
-	"devotion",
-	"intimacy",
-	"kiss",
-	"hug",
-	"embrace",
-	"affection",
-	"adore",
-	"treasure",
-	"commitment",
-	"together",
-	"unity",
-	"bond",
-	"harmony",
-	"loyalty",
-	"dedication",
-	"adoration",
-	"infatuation",
-	"yearning",
-	"longing",
-	"bliss",
-	"passionately",
-	"endlessly",
-	"unconditionally",
-	"completely",
-	"totally",
-	"absolutely",
-	"entirely",
-	"deeply",
-	"truly",
-	"madly",
-	"wildly",
-	"sincerely",
-	"passionately",
-	"tenderly",
-	"intensely",
-	"amazingly"
+const poems = [
+  "Roses are red, violets are blue, being with you makes me feel like anything is possible, Yashika.",
+  "I never knew that I could love someone as much as I love you, Yashika. You have truly changed my life for the better.",
+  "When I'm with you, Kaustav, everything else fades away. You are the only one that matters to me.",
+  "I am so grateful to have you in my life, Yashika. I don't know what I would do without you.",
+  "You are the sun in my sky, Yashika. You brighten up my life and make everything better.",
+  "I love you more every day, Kaustav. You are the missing piece that completes me."
 ];
 
-const poemTemplates = [
-	`${kaustav}, my ${loveWord}, my ${loveWord},
-	You are the ${loveWord} that fills my heart.
-	${yashika}, my ${loveWord}, my ${loveWord},
-	You are the ${loveWord} that makes me smart.`
-	${kaustav}, my ${loveWord}, my ${loveWord},
-	With you, I am never alone.
-	${yashika}, my ${loveWord}, my ${loveWord},
-	Our love is forever known.
-	
-	${kaustav}, my ${loveWord}, my ${loveWord},
-	You are the ${loveWord} that lights up my life.
-	${yashika}, my ${loveWord}, my ${loveWord},
-	You are the ${loveWord} that ends all strife.
-	
-	${kaustav}, my ${loveWord}, my ${loveWord},
-	Together we can conquer anything.
-	${yashika}, my ${loveWord}, my ${loveWord},
-	In your arms is where my heart takes wing.
-`;
+const poemContainer = document.getElementById("poem-container");
+const generateButton = document.getElementById("generate-button");
 
-const generatePoem = () => {
-	const randomName1 = Math.random() < 0.5 ? kaustav : yashika;
-	const randomName2 = Math.random() < 0.5 ? kaustav : yashika;
-	const randomLoveWord = loveWords[Math.floor(Math.random() * loveWords.length)];
-	const randomTemplate = poemTemplates[Math.floor(Math.random() * poemTemplates.length)];
-	
-	const poem = randomTemplate
-		.replace(/kaustav/g, randomName1)
-		.replace(/yashika/g, randomName2)
-		.replace(/loveWord/g, randomLoveWord);
-	
-	document.getElementById("poem-container").innerHTML = `<p>${poem}</p>`;
-}
-
-document.getElementById("generate-btn").addEventListener("click", generatePoem);
+generateButton.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * poems.length);
+  const randomPoem = poems[randomIndex];
+  poemContainer.innerHTML = `<p>${randomPoem}</p>`;
+});
